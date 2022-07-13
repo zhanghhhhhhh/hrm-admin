d23aab5751d397ba561fc099ed64fd0291d89fff
itzhang <1745036118@qq.com>
2022-07-13 11:51:07 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '人力资源管理后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 设置appMain右侧字体上面的header是不是固定定位
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 设置侧边栏上面的logo是否设置
+   */
+  sidebarLogo: false
+}
